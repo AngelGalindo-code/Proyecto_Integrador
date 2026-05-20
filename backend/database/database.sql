aaa450dc5d26613b4c1e357e04132707109d7480
Yhoan <cruzyhoan6@gmail.com>
2026-05-19 23:19:51 -0300

    agrego tabla reservas con relacion a tabla usuarios

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -8,3 +8,10 @@ CREATE TABLE usuarios(
     email VARCHAR(50) UNIQUE NOT NULL,
     rol VARCHAR(50) NOT NULL
 );
+CREATE TABLE reservas(
+    id_reserva INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_usuario INT NOT NULL,
+    fecha DATETIME NOT NULL,
+
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
+);
