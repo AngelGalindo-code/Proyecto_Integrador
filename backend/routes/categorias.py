68d995d5088ca3ef10716358657cf5c388164274
Lautaro Nehuen Rodriguez <lnrodriguez@fi.uba.ar>
2026-05-19 23:15:01 -0300

    Agrego el endpoint POST que crea una nueva categoria

--- backend/routes/categorias.py
+++ backend/routes/categorias.py
@@ -136,3 +136,75 @@ def obtener_nombres_categorias():
             conn.close()
 
 
+@categorias_bp.route("/", methods=["POST"]) #usuario admin
+def agregar_categoria():
+    data = request.json
+    nombre_categoria = data.get("nombre_categoria")
+
+    if (
+        not nombre_categoria
+        or not isinstance(nombre_categoria, str)
+        or not nombre_categoria.strip()
+    ):
+        error_400 = {
+            "errors": [
+                {
+                    "code": "400",
+                    "message": "Bad Request",
+                    "level": "error",
+                    "description": "No se ha mandado el nombre de la categoria o se mandado un dato invalido",
+                }
+            ]
+        }
+        return jsonify(error_400), 400
+
+    conn = None
+    cur = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor(dictionary=True)
+
+        query_validar_existencia_id = """
+        SELECT * FROM categorias 
+WHERE nombre_categoria = %s      
+"""
+        cur.execute(query_validar_existencia_id, (nombre_categoria,))
+        categoria = cur.fetchone()
+        if categoria:
+            error_409 = {
+                "errors": [
+                    {
+                        "code": "409",
+                        "message": "Conflict",
+                        "level": "error",
+                        "description": "La categoria ya existe",
+                    }
+                ]
+            }
+            return jsonify(error_409), 409
+
+        querey_insertar_valores = """
+INSERT INTO categorias (nombre_categoria) VALUES
+                    (%s)
+"""
+        cur.execute(querey_insertar_valores, (nombre_categoria,))
+        conn.commit()
+
+        return "", 201
+    except Exception as e:
+        error_500 = {
+            "errors": [
+                {
+                    "code": "500",
+                    "message": "Internal Server Error",
+                    "level": "error",
+                    "description": str(e),
+                }
+            ]
+        }
+        return jsonify(error_500), 500
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
