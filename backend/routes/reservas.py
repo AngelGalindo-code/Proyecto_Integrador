19ccc74d19c67e7bc3916af01bf15bec8c48a950
Franco Lema <120995819+FrancoLema99@users.noreply.github.com>
2026-05-23 01:50:29 -0300

    Agrego carpeta 'validators' con querys y db y modifico endpoints
    Updated the reservas.py file to use db_sqlalchemy for database interactions and improved error handling in various routes.

--- backend/routes/reservas.py
+++ backend/routes/reservas.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Blueprint, flash, request, redirect, url_for
-from validators.db import *
+from validators.db_sqlalchemy import *
 
-# Se toman funciones de validaciones de un archivo llamado db.py
+# Se toman funciones de validaciones de un archivo llamado db_sqlalchemy.py
 # Se toma el nombre de templates que validan errores, listas, etc
 # Falta la validacion de errores
 
@@ -80,64 +80,70 @@ def adminReservas():
         )
 
 # Copiando el modelo del backend del trabajo del fixture 
+# Aun se encuentra incompleto. Ver implementacion del try-except
 @reservas_bp.route('/', methods=['GET'])
 def reservasList():
 
-    try:
-        reservas = getReservas()
-    
-    except Exception as e:
-        return render_template('sinreservas.html', message='No se Encontraron reservas')
+    reservas = getReservas()
 
-    return render_template('lista.html', title='Reservas', reservas=reservas)
+    if not reservas:
+        flash('No hay reservas!')
+
+        return render_template('templates/errors/sinreservas.html', title='Error')
+    
+    else:
+        flash('Aca se encuentra la lista de todas las reservas')
+
+        return render_template('templates/lista.html', title='Reservas')
+
 
 @reservas_bp.route('/<id_reserva>/mireserva', methods=['GET'])
 def getReservaPorId(id_reserva):
 
-    try:
-        id_reserva = int(id_reserva)
+    miReserva = getReservaPorId(id_reserva) # Ver implementacion del try-except y validacion de id
 
-    except ValueError:
-        return render_template('errorGenerico.html', message='Error de datos')
+    if not miReserva:
+        flash('No hay reservas con ese ID')
+
+        return render_template('errors/404_notFound.html')
     
+    return render_template('miReserva.html', title='Mi reserva')
+
+
+
+
+@reservas_bp.route('/<id_reserva>/modificar', meethods=['GET', 'POST']) 
+def modificarRerserva(id_reserva):
+
+    # Validar si se pasa id incorrecto
+
+
+    # Hacer el formulario html!!!!!!
+    if request.method == 'POST':
+
+        actualizada = actualizarReserva(
+            nombre = request.form.get('nombre'),
+            fecha = request.form.get('fecha'),
+            mesa = request.form.get('mesa'),
+            cantidad_personas = request.form.get('cantidad_personas'),
+            id_reserva = id_reserva
+        )
+
+        if actualizada:
+            return redirect(url_for('.getReservaPorId', id_reserva=id_reserva))
+    
+
+        else:
+            return render_template('errors/404_notFound.html')
+    
+
     miReserva = getReservaPorId(id_reserva)
 
     if not miReserva:
-        return render_template('404_notfound.html', message=f'No se pudo encontrar tu reserva con el id de reserva: {id_reserva}')
+        return render_template('errors/404_notFound.html')
     
-    return render_template('miReserva.html', title='Mi Reserva', reserva=miReserva)
+    return render_template('formularioReserva.html')
 
-
-
-@reservas_bp.route('/<id_reserva>/modificar', methods=['GET', 'POST'])
-def modificarRerserva(id_reserva):
-
-    try:
-        id_reserva = int(id)
-    except ValueError:
-        flash("El ID de la reserva proporcionado no es válido.")
-        return redirect(url_for('reservas.reservasList'))
-
-    form_data = request.form
-
-    try:
-        modificarReserva(
-            id_reserva=id_reserva,
-            fecha=form_data.get('fecha'),
-            hora=form_data.get('hora'),
-            mesa=int(form_data.get('mesa')),
-            cantidad_personas=int(form_data.get('cantidad_personas'))
-        )
-        flash('Reserva modificada correctamente.', 'info')
-        
-    except ValueError as e:
-        # Ver trabajo de fixture
-        mensaje_error = e.args[0] if e.args else "Error al modificar la reserva."
-        flash(mensaje_error)
-        # Por si falla, me manda de nuevo a reservar
-        return redirect(url_for('reservasList'))
-
-    return redirect(url_for('reservasList'))
 
 
 @reservas_bp.route('/reservar', methods=['POST'])
