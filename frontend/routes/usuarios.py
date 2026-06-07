e0d13734fb94ef2672fa63a60e4f9b3b85d0bd5d
Angel Galindo <agalindo@fi.uba.ar>
2026-06-07 02:49:43 -0300

    crear admin_bp en nuevo archivo

--- frontend/routes/usuarios.py
+++ frontend/routes/usuarios.py
@@ -110,6 +110,7 @@ def actualizar_completamente_usuario(id):
     except Exception:
         return render_template('errorGenerico.html', message='Error al actualizar usuario')
     
+@usuarios_bp.route('/usuarios/<int:id>', methods=['POST'])
 
 def actualizar_parcialmente_ususario(id):
     
@@ -145,105 +146,3 @@ def actualizar_parcialmente_ususario(id):
     except Exception:
         return render_template('errorGenerico.html', message='Error al actualizar usuario')
    
-   
-@usuarios_bp.route('/usuarios/<id>/eliminar', methods=['POST'])
-
-def eliminarUsuario(id):
-
-    try:
-
-        id = int(id)
-
-        if session.get('rol') != 'admin':
-
-            flash('No tiene permisos para eliminar usuarios')
-
-            return redirect('/') 
-
-        eliminado = eliminarUsuarioPorId(id)
-
-        if not eliminado:
-
-            flash('No existe un usuario con ese ID :(')
-
-            return render_template('errors/404_notFound.html')# -> reemplazo de jsonify por render_template y manejo de errores mediante html reutilizable
-        # PD: con fran mantuvimos la estructura asi en reservas.py 
-
-        flash(
-            'Usuario eliminado correctamente')
-
-        return redirect(
-            url_for('usuarios.adminUsuarios'))
-
-    except ValueError:
-
-        flash('El ID es invalido')
-
-        return render_template('errors/404_notFound.html') 
-
-    except Exception:
-        return render_template('errorGenerico.html', message='Error al eliminar usuario')
-        
-        
-         
-@usuarios_bp.route('/admin/usuarios', methods=['GET'])
-
-def adminUsuarios():
-
-    try:
-
-        if session.get('rol') != 'admin':
-
-            flash('Acceso denegado')
-
-            return redirect('/')
-
-        usuarios = getUsuarios()
-
-        if not usuarios:
-
-            flash('No hay usuarios registrados')
-
-            return render_template('errors/sinusuarios.html')
-
-        return render_template('adminUsuarios.html', title='Usuarios',usuarios=usuarios)
-
-    except Exception:
-        return render_template('errorGenerico.html',message='Error al obtener usuarios')       
-        
-        
-        
-            
-@usuarios_bp.route('/admin/usuarios/<id>', methods=['GET'])
-
-def adminUsuarioPorId(id):
-
-    try:
-
-        id = int(id)
-
-        if session.get('rol') != 'admin':
-
-            flash('Acceso denegado')
-
-            return redirect('/')
-
-        usuario = getUsuarioPorId(id)
-
-        if not usuario:
-
-            flash('El usuario no fue encontrado')
-
-            return render_template('errors/404_notFound.html')
-
-        return render_template('adminUsuario.html',title='Usuario', usuario=usuario)
-
-    except ValueError:
-
-        flash('El ID es invalido')
-
-        return render_template('errors/404_notFound.html')
-
-    except Exception:
-        return render_template('errorGenerico.html', message='Error al obtener el usuario')
-    
