2bf43ec4008e18c4066773cf22f61880b8c2306b
FrancoLema99 <francolema1@gmail.com>
2026-06-15 00:30:34 -0300

    Agregando endpoints de reservas usando requests, y mejorando htlms y JS

--- frontend/routes/reservas.py
+++ frontend/routes/reservas.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template, Blueprint, flash, request, redirect, url_for, abort, session 
-<<<<<<< HEAD
 from routes.decorators import adminRequired, loginRequired
-from database.db_reservas import *
 from validaciones.reservas import *
-
-
-
-# Se toman funciones de validaciones de un archivo llamado db_sqlalchemy.py
-# Se toma el nombre de templates que validan errores, listas, etc
-# Falta la validacion de errores
-=======
+import requests 
 from constantes import URL_BACKEND
-from decorators.decorators import adminRequired, loginRequired
->>>>>>> 230454bb48af12ef34103c6db7f1f7fce9148598
+
+# Para el QR
+import qrcode
+import io
+import base64
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+
+apiBackend = requests.Session()
+
 
 reservas_bp = Blueprint("reservas", __name__)
 
-# Se toma el nombre de templates que validan errores, listas, etc
-# Falta la validacion de errores
-# Implementar el uso de sessions
+@reservas_bp.errorhandler(405)
+def method_not_allowed(e):
 
-# Errores ---> Ver manejo de errores con BluePrint ---> Errores especificos segun la ruta /reservas
+    return render_template('400.html'), 405
 
 @reservas_bp.errorhandler(500)
 def internal_server_error(e):
@@ -39,105 +44,39 @@ def bad_request(e):
     return render_template('errors/400_badRequest.html'), 400
 
 
-@reservas_bp.route("/hacer-reserva", methods=["GET"])
-def mostrarFormulario():
-
-    return render_template(
-        "formulario-reserva.html",
-        subtitulo_reserva = "Reserva Ahora",
-        titulo_principal = "Haz una reserva",
-        horario ="Martes a Domingos de 8:00h a 23:00h",
-        direccion = "Calle Pepito 123, Buenos Aires")
-
-@reservas_bp.route("/reservas", methods=["POST"])
-def crearReserva():
-
-    if not request.form:
-        flash("Sin informacion en el formulario", "danger")
-        return redirect(url_for("reservas.mostrarFormulario")), 400
-    
-    try:    
-        from_data = request.form
-
-        id_usuario = session.get("id_usuario")
-        cantidad_personas = from_data.get("cantidad_personas")
-        mesa = from_data.get("mesa")
-
-        input_fecha_hora = from_data.get("fecha_hora")
-        if input_fecha_hora and "T" in input_fecha_hora:
-            fecha, hora = input_fecha_hora.split("T")
-        
-        else:
-            fecha, hora = None, None
-
-        if not all([id_usuario, fecha, hora, mesa, cantidad_personas]):
-            flash("Faltan datos", "danger")
-            return redirect(url_for("reservas.mostrarFormulario")), 400
-        
-        payload = {
-            "id_usuario": id_usuario,
-            "fecha": fecha,
-            "hora": hora,
-            "mesa": mesa,
-            "cantidad_personas": cantidad_personas
-        }
-
-        respuesta = request.post(f"{URL_BACKEND}/reservas", json=payload)
-
-        if respuesta.status_code == 201:
-            flash("Reserva hecha", "success")
-            return redirect(url_for("reservas.mostrarFormulario"))
-        
-        if respuesta.status_code == 409:
-            flash("Mesa ya reservada para esa fecha y hora", "warning")
-            return redirect(url_for("reservas.mostrarFormulario"))
-        
-        flash("No se pudo crear la reserva", "danger")
-        return redirect(url_for("reservas.mostrarFormulario"))
-    
-    except Exception as e:
-        print(f"error en la conexion: {e}")
-        flash("No pudimos procesar tu reserva, intente otra vez")
-        return redirect(url_for("reservas.mostrarFormulario")), 500
-
-#Ver reservas admin
-@reservas_bp.route('/admin/reservas', methods=['GET'])
-def adminReservas():
-
-    try:
-        reservas = getReservas()
-        
-        if not reservas:
-
-            return render_template(
-                'sinreservas.html',
-                message='No existen reservas'
-            )
-
-        return render_template(
-            'adminReservas.html',
-            title='Reservas',
-            reservas=reservas
-        )
-
-    except Exception:
-
-        return render_template(
-            '500_serverError.html',
-            message='Error al obtener reservas'
-        )
 
 @reservas_bp.route('/admin', methods=['GET'])
 @adminRequired
 def reservasList():
 
-    reservas = getReservas()
+    try:
+        respuesta = apiBackend.get(f"{URL_BACKEND}/reservas/admin", timeout=5)
 
-    if not reservas:
-        return render_template('listaVacia.html')
-   
-    return render_template('lista.html', title='Reservas', reservas=reservas)
+        if respuesta.status_code == 404:
+            abort(404)
+        
+        if respuesta.status_code == 403:
+            abort(403)
 
+        if respuesta.status_code == 400:
+            abort(400)
+
+        if respuesta.status_code == 200:
+            reservas = respuesta.json()
+            
+            if not reservas:
+                flash("No se encontraron reservas")
+                return render_template("listaVacia.html")
+            
+            flash("Aca se encuentra el listado de todas las reservas")
+            return render_template("listas.html", reservas=reservas)
+        
+        else:
+            abort(500)
+        
+    except requests.exceptions.RequestException:
+        abort(500)
+        
 
 @reservas_bp.route('/<int:id_reserva>/mireserva', methods=['GET'])
 @loginRequired
@@ -148,66 +87,165 @@ def getReservaPorId(id_reserva):
     if not id_valido:
         abort(404)
 
-    miReserva = obtenerReservaPorId(id_reserva)
-    
-    if not miReserva:
-        abort(404)
+    try:
+        respuesta = apiBackend.get(f"{URL_BACKEND}/reservas/{id_reserva}/mireserva", timeout=5)
 
-    if session.get('id_usuario') != miReserva.get('id_usuario'):
-        abort(403)
+        if respuesta.status_code == 400:
+            abort(400)
+
+        if respuesta.status_code == 404:
+            abort(404)
+
+        if respuesta.status_code == 200:
+
+            miReserva = respuesta.json()
+            if session.get('id_usuario') != miReserva.get('id_usuario'):
+                abort(403)
+            
+            flash("Estos son los detalles de su reserva", "success")
+            return render_template("miReserva.html", title="Mi reserva", reserva=miReserva)
+        
+        else:
+            abort(500)
     
-    return render_template('miReserva.html', title='Mi reserva', reserva=miReserva)
+    except requests.exceptions.RequestException:
+        abort(500)
+            
+            
 
 
 
 
 @reservas_bp.route('/<id_reserva>/modificar', methods=['GET', 'POST']) 
+@loginRequired
 def modificarRerserva(id_reserva):
 
     id_valido = validarId(id_reserva)
+
     if not id_valido:
-        return render_template('errors/404_notFound.html')
-    
-    if request.method == 'POST':
+        abort(400)
 
-        actualizada = actualizarReserva(
-            nombre = request.form.get('nombre'),
-            fecha = request.form.get('fecha'),
-            mesa = request.form.get('mesa'),
-            cantidad_personas = request.form.get('cantidad_personas'),
-            id_reserva = id_reserva
-        )
+    try:
+        if request.method == "POST":
+                verificada = apiBackend.get(f"{URL_BACKEND}/reservas/{id_reserva}", timeout=5)
+                
+                if verificada.status_code == 404:
+                    abort(404)
+   
+                reserva = verificada.json()
 
-        if actualizada:
-            return redirect(url_for('.getReservaPorId', id_reserva=id_reserva))
-    
+                if session.get('id_usuario') != reserva.get('id_usuario'):
+                    abort(403)
+                
+                payload = {
+                    'nombre' : request.form.get('nombre', '').strip(),
+                    'fecha' : request.form.get('fecha', '').strip(),
+                    'mesa' : request.form.get('mesa').strip(),
+                    'cantidad_personas' : request.form.get('cantidad_personas', '').strip()
+                }  
+        
+                reservaActualizada = apiBackend.put(f"{URL_BACKEND}/reservas/{id_reserva}/modificar", json=payload, timeout=5)
+
+                if reservaActualizada.status_code == 200:
+                    flash("Reserva actualizada con exito", "success")
+                    return redirect(url_for(".getReservaPorId", id_reserva=id_reserva))
+        
+                else:
+                    abort(500)
+
+        
+        respuesta = apiBackend.get(f"{URL_BACKEND}/reservas/{id_reserva}/modificar", timeout=5)
+
+        if respuesta.status_code == 404:
+            abort(404)
+
+        if respuesta.status_code == 200:
+            miReserva = respuesta.json()
+
+            if session.get('id_usuario') != miReserva.get('id_usuario'):
+                abort(403)
+
+            return render_template("modificarReserva.html", reserva=miReserva)
+        
         else:
-            return render_template('errors/404_notFound.html')
-    
-    miReserva = getReservaPorId(id_reserva)
+            abort(500)
 
-    if not miReserva:
-        return render_template('errors/404_notFound.html')
-    
-    return render_template('formularioReserva.html', reserva=miReserva)
+    except requests.exceptions.RequestException:
+        abort(500)
 
-
-
-@reservas_bp.route('/<id_reserva>/eliminar', methods=['POST'])
+@reservas_bp.route('/<int:id_reserva>/eliminar', methods=['POST'])
 @loginRequired
 def cancelarReserva(id_reserva):
     idValido = validarId(id_reserva)
+    if not idValido:
+        abort(400)
+    
+    try:
+        verificacion = apiBackend.get(f"{URL_BACKEND}/reservas/{id_reserva}", timeout=5)
+        if verificacion.status_code == 404:
+            abort(404)
+            
+        miReserva = verificacion.json()
+        if session.get('id_usuario') != miReserva.get('id_usuario'):
+            abort(403) 
 
+        respuesta = apiBackend.delete(f"{URL_BACKEND}/reservas/{id_reserva}", timeout=5)
+        
+        if respuesta.status_code == 200:
+            flash('Reserva eliminada correctamente', 'success')
+            return redirect(url_for('home')) 
+        else:
+            abort(500)
+            
+    except requests.exceptions.RequestException:
+        abort(500)
+
+
+
+@reservas_bp.route('/<int:id_reserva>/exito', methods=['GET'])
+@loginRequired
+def reservaExitosa(id_reserva):
+
+    idValido = validarId(id_reserva)
     if idValido == False:
         abort(400)
     
-    miReserva = obtenerReservaPorId(id_reserva)
-    if not miReserva:
-        abort(404)
+    try:
+        sesion = apiBackend.get(f"{URL_BACKEND}/reservas/{id_reserva}", timeout=5)
 
-    if session.get('id_usuario') != miReserva.get('id_usuario'):
-        abort(403)
-
-    eliminado = eliminarReserva(id_reserva)
+        if sesion.status_code == 404:
+            abort(404)
         
-    return redirect(url_for('reservas.crearReserva'))
+        if sesion.status_code == 200:
+            miReserva = sesion.json()
+
+            if session.get('id_usuario') != miReserva.get('id_usuario'):
+                abort(403)
+
+            informacion_qr = (
+                f"""--- CONFIRMACIÓN DE RESERVA ---\n
+                ID de Reserva: {miReserva.get('id')}\n
+                Nombre: {miReserva.get('nombre')}\n
+                Fecha: {miReserva.get('fecha')}\n
+                Hora: {miReserva.get('hora')} hs\n
+                Personas: {miReserva.get('personas')} personas"""
+            )
+            
+            qr.add_data(informacion_qr)
+            qr.make(fit=True)
+            imagen_qr = qr.make_image(fill_color="black", back_color="white")
+            buffer_memoria = io.BytesIO()
+            imagen_qr.save(buffer_memoria, format="PNG")
+            buffer_memoria.seek(0)
+            qr_base64 = base64.b64encode(buffer_memoria.read()).decode('utf-8')
+            qr.clear()
+
+            return render_template('reserva_exito.html', reserva=miReserva, qr_code=qr_base64)
+        
+        else:
+            abort(500)
+
+    except requests.exceptions.RequestException:
+        abort(500)
+
+# Ver Flask-email para el envio del qr, o que solo que en la vista.
