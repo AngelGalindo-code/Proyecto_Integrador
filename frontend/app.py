0e324057429528ae029ceae5daa7e2628dcca180
Angel Galindo <agalindo@fi.uba.ar>
2026-06-16 17:45:56 -0300

    Ubicar menu.html arriba del formulario de reserva en el home y pasarle una lista vacia en el app.py para que se renderize el home

--- frontend/app.py
+++ frontend/app.py
@@ -25,7 +25,8 @@ def index():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    
+    return render_template('home.html', menu={'comidas': []})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
