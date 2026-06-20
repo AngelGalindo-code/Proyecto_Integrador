d2952d37a00f99c7483cd12d7871941438a43f09
Lautaro Nehuen Rodriguez <lnrodriguez@fi.uba.ar>
2026-06-20 14:00:57 -0300

    Agrego validacion de valoracion

--- frontend/static/js/publicar_resena.js
+++ frontend/static/js/publicar_resena.js
@@ -1,12 +1,10 @@
-//iluminar estrellas y las validaciones
+//iluminar estrellas
 
 const estrellas = document.getElementById("estrellas")
 const estrellitas = document.getElementsByClassName("iluminable")
 
-
 estrellas.addEventListener("click", function(event) {
     iluminar_estrellas()
-    //faltan los posibles errores
 })
 
 function iluminar_estrellas(){
@@ -32,14 +30,12 @@ function encontrar_llamado(){
     return n
 }
 
-//cerrar formulario
-
-function cerrarResena() { 
-    document.getElementById("formulario_resena").style.display = "none"
-}
+//cerrar formulario y las validaciones
 
 cancelar_posteo = document.getElementById("cerrar_ventana")
 publicar_posteo = document.getElementById("boton_publicar")
+
+const errores = document.getElementsByClassName("error")
 
 cancelar_posteo.addEventListener("click", function(event) {
     cerrarResena()
@@ -47,5 +43,29 @@ cancelar_posteo.addEventListener("click", function(event) {
 
 
 publicar_posteo.addEventListener("click", function(event) {
-    cerrarResena()
+      event.preventDefault()
+
+    while (errores.length > 0) {
+        errores[0].remove()
+    }
+
+    if (encontrar_llamado() == 0) {
+        error_valoracion("Debe dejar una valoración", estrellas)
+    }
+   
+    if (errores.length == 0) {
+        event.target.form.submit()
+        cerrarResena()
+    }
 })
+
+function cerrarResena() { 
+    document.getElementById("formulario_resena").style.display = "none"
+}
+
+function error_valoracion(mensaje, contenedor) {
+    const error = document.createElement("span")
+    error.textContent = mensaje
+    error.classList.add("error")
+    contenedor.appendChild(error)
+}
