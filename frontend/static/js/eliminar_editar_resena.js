a844970dd8b943cf69dcff819831aa94be23bd40
Lautaro Nehuen Rodriguez <lnrodriguez@fi.uba.ar>
2026-06-23 17:29:12 -0300

    Agrego en js evento para iluminar las estrellas

--- frontend/static/js/eliminar_editar_resena.js
+++ frontend/static/js/eliminar_editar_resena.js
@@ -19,3 +19,43 @@ for (let i=0; i < cancelar_modificar.length; i++) {
 function cerrar_resena(formulario, i) { 
     document.getElementsByClassName(formulario)[i].style.display = "none"
 }
+
+
+
+// Iluminar estrellas
+const cont_estrellas = document.getElementsByClassName("contenedor_estrellas")
+
+for (let i = 0; i < cont_estrellas.length; i++) {
+    cont_estrellas[i].addEventListener("click", function(event) {
+        iluminar_estrellas(cont_estrellas[i])
+    }
+)}
+
+
+function iluminar_estrellas(cont_estrellas){
+    let n = encontrar_llamado(cont_estrellas)
+
+    const estrellitas = cont_estrellas.getElementsByClassName("iluminable")
+
+    for (let i=0; i<estrellitas.length; i++){
+        if (i < n) {
+            estrellitas[i].textContent = "★";
+        } else {
+            estrellitas[i].textContent = "☆";
+        }
+    }
+
+}
+
+function encontrar_llamado(cont_estrellas){ 
+    let n = 0
+
+    const estrellas = cont_estrellas.getElementsByClassName("estrellas")
+
+    for (let i=0; i<estrellas.length; i++){
+        if (estrellas[i].checked){
+            n = i + 1
+        }
+    }
+    return n
+}
