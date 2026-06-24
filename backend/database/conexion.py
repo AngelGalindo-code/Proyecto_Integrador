599653b6421b11e20d43c9f76b3757c28d4b92e3
Atila <atilajmsousa@gmail.com>
2026-06-24 00:07:47 -0300

    fix: volver a usar aiven y cambios en docker-compose

--- backend/database/conexion.py
+++ backend/database/conexion.py
@@ -1,31 +1,23 @@
 import os
 import pymysql
-from pymysql.cursors import DictCursor #devuelve datos en forma de diccionarios clave valor
+from pymysql.cursors import DictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
     try:
-        # desde .env obtenemos el host de la base de datos (puede ser 'db' para docker o un host externo)
-        host = os.getenv("DB_HOST")
-        
-        config = {
-            "host": host,
-            "port": int(os.getenv("DB_PORT", 3306)),
-            "user": os.getenv("DB_USER"),
-            "password": os.getenv("DB_PASSWORD"),
-            "database": os.getenv("DB_NAME"),
-            "cursorclass": DictCursor
-        }
-
-        # usamos SSL solo si estamos en un entorno externo
-        if host != "db":
-            config["ssl"] = {"ssl_ca": "certificates/ca.pem"}
-
-        connection = pymysql.connect(**config)
+        connection = pymysql.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT", 10599)),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            ssl={"ssl_ca": "certificates/ca.pem"},
+            cursorclass = DictCursor
+        )
         return connection
         
     except pymysql.MySQLError as e:
-        print(f"Error al conectar a MySQL: {e}")
-        return None
+        print(f" Error al conectar a MySQL en Aiven: {e}")
+        return e
