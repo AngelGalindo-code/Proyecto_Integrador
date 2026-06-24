fe8e2dfa4fb816240a1a4a9a9a1f4888977084f3
Atila <atilajmsousa@gmail.com>
2026-06-23 23:43:09 -0300

    Dockerizo el proyecto

--- backend/database/conexion.py
+++ backend/database/conexion.py
@@ -6,23 +6,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    """
-    Establece la conexion con el servidor MySQL de Aiven en la nube
-    utilizando SSL obligatorio. 
-    """
     try:
-        connection = pymysql.connect(
-            host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT", 10599)),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME"),
-            ssl={"ssl_ca": "certificates/ca.pem"},    # activa el modo SSL obligatorio que pide Aiven
-            cursorclass = DictCursor
-        )
+        # desde .env obtenemos el host de la base de datos (puede ser 'db' para docker o un host externo)
+        host = os.getenv("DB_HOST")
+        
+        config = {
+            "host": host,
+            "port": int(os.getenv("DB_PORT", 3306)),
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "database": os.getenv("DB_NAME"),
+            "cursorclass": DictCursor
+        }
 
+        # usamos SSL solo si estamos en un entorno externo
+        if host != "db":
+            config["ssl"] = {"ssl_ca": "certificates/ca.pem"}
+
+        connection = pymysql.connect(**config)
         return connection
         
     except pymysql.MySQLError as e:
-        print(f" Error al conectar a MySQL en Aiven: {e}")
-        return e
+        print(f"Error al conectar a MySQL: {e}")
+        return None
--- frontend/constantes.py
+++ frontend/constantes.py
@@ -1,1 +1,1 @@
-URL_BACKEND = "http://127.0.0.1:5000"
+URL_BACKEND = "http://backend:5000"
